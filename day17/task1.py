57b9a0b8fdb7f25d799f31f0ca7cd37f91e618cf
Vimor123 <vmladar1@gmail.com>
2023-12-17 18:08:53 +0100

    Maybe

--- day17/task1.py
+++ day17/task1.py
@@ -4,26 +4,13 @@ sys.setrecursionlimit(100000000)
 
 heat_loss_map = []
 
-distances_map = []
-
-input_file = open("input1.txt", "r")
+input_file = open("input.txt", "r")
 
 for line in input_file:
     heat_loss_map.append([int(x) for x in list(line.strip())])
 
-    distances_map.append([])
-    for i in range(len(line.strip())):
-        distances = {}
-        for straights_remaining in range(3):
-            for direction in ["left", "up", "right", "down"]:
-                distances[(straights_remaining, direction)] = -1
-        distances_map[-1].append(distances)
-
 input_file.close()
 
-for straights_remaining in range(3):
-            for direction in ["left", "up", "right", "down"]:
-                distances_map[0][0][(straights_remaining, direction)] = 0
 
 turning = {
         "left" : { "turn_left" : "down", "turn_right" : "up" },
@@ -40,64 +27,87 @@ increments = {
 }
 
 
+nodes_to_visit = [{ "distance" : 0, "position" : (0, 0), "direction" : "right", "remaining_straights" : 3 },
+                  { "distance" : 0, "position" : (0, 0), "direction" : "down", "remaining_straights" : 3 }]
+
+
 def position_on_map(position):
     if position[0] < 0 or position[0] >= len(heat_loss_map) or position[1] < 0 or position[1] >= len(heat_loss_map):
         return False
     return True
 
+iteration = 0
 
-def search_path(current_loss, position, direction, straights_remaining):
-    left_valid = False
-    right_valid = False
-    straight_valid = False
+visited = {}
+
+while len(nodes_to_visit) > 0:
+    min_distance_node_index = -1
+    min_distance = -1
+    for node_index, node in enumerate(nodes_to_visit):
+        if node["distance"] < min_distance or min_distance < 0:
+            min_distance = node["distance"]
+            min_distance_node_index = node_index
+
+    node = nodes_to_visit.pop(min_distance_node_index)
+
+    node_without_distance = (node["position"], node["direction"], node["remaining_straights"] )
+
+    if node_without_distance in visited:
+        continue
+
+    visited[node_without_distance] = node["distance"]
 
     # Turn left
-    new_direction_1 = turning[direction]["turn_left"]
+    new_direction_1 = turning[node["direction"]]["turn_left"]
     increment = increments[new_direction_1]
-    new_position_1 = (position[0] + increment[0], position[1] + increment[1])
+    new_position_1 = (node["position"][0] + increment[0], node["position"][1] + increment[1])
+
     if position_on_map(new_position_1):
-        new_loss = current_loss + heat_loss_map[new_position_1[0]][new_position_1[1]]
-        if new_loss < distances_map[new_position_1[0]][new_position_1[1]][(2, new_direction_1)] or distances_map[new_position_1[0]][new_position_1[1]][(2, new_direction_1)] < 0:
-            for i in range(3):
-                distances_map[new_position_1[0]][new_position_1[1]][(i, new_direction_1)] = new_loss
-            search_path(new_loss, new_position_1, new_direction_1, 2)
-            
+        new_node = {
+            "distance" : node["distance"] + heat_loss_map[new_position_1[0]][new_position_1[1]],
+            "position" : new_position_1,
+            "direction" : new_direction_1,
+            "remaining_straights" : 2
+        }
+        nodes_to_visit.append(new_node)
 
     # Turn right
-    new_direction_2 = turning[direction]["turn_right"]
+    new_direction_2 = turning[node["direction"]]["turn_right"]
     increment = increments[new_direction_2]
-    new_position_2 = (position[0] + increment[0], position[1] + increment[1])
+    new_position_2 = (node["position"][0] + increment[0], node["position"][1] + increment[1])
+
     if position_on_map(new_position_2):
-        new_loss = current_loss + heat_loss_map[new_position_2[0]][new_position_2[1]]
-        if new_loss < distances_map[new_position_2[0]][new_position_2[1]][(2, new_direction_2)] or distances_map[new_position_2[0]][new_position_2[1]][(2, new_direction_2)] < 0:
-            for i in range(3):
-                distances_map[new_position_2[0]][new_position_2[1]][(i, new_direction_2)] = new_loss
-            search_path(new_loss, new_position_2, new_direction_2, 2)
+        new_node = {
+            "distance" : node["distance"] + heat_loss_map[new_position_2[0]][new_position_2[1]],
+            "position" : new_position_2,
+            "direction" : new_direction_2,
+            "remaining_straights" : 2
+        }
+        nodes_to_visit.append(new_node)
 
     # Continue straight
-    increment = increments[direction]
-    new_position_3 = (position[0] + increment[0], position[1] + increment[1])
-    if position_on_map(new_position_3) and straights_remaining > 0:
-        new_loss = current_loss + heat_loss_map[new_position_3[0]][new_position_3[1]]
-        new_straights_remaining = straights_remaining - 1
-        if new_loss < distances_map[new_position_3[0]][new_position_3[1]][(new_straights_remaining, direction)] or distances_map[new_position_3[0]][new_position_3[1]][(new_straights_remaining, direction)] < 0:
-            for i in range(new_straights_remaining + 1):
-                distances_map[new_position_3[0]][new_position_3[1]][(i, direction)] = new_loss
-            search_path(new_loss, new_position_3, direction, new_straights_remaining)
+    increment = increments[node["direction"]]
+    new_position_3 = (node["position"][0] + increment[0], node["position"][1] + increment[1])
 
+    if position_on_map(new_position_3) and node["remaining_straights"] > 0:
+        new_node = {
+            "distance" : node["distance"] + heat_loss_map[new_position_3[0]][new_position_3[1]],
+            "position" : new_position_3,
+            "direction" : node["direction"],
+            "remaining_straights" : node["remaining_straights"] - 1
+        }
+        nodes_to_visit.append(new_node)
 
+    iteration += 1
 
+    if iteration % 10000 == 0:
+        print(iteration, len(nodes_to_visit))
 
-
-loss = 0
-search_path(loss, (0, 0), "right", 3)
-search_path(loss, (0, 0), "down", 3)
 
 min_distance = -1
-
-for distance in distances_map[-1][-1].values():
-    if distance > 0:
-        if min_distance < 0 or min_distance > distance:
+for node, distance in visited.items():
+    if node[0][0] == len(heat_loss_map) - 1 and node[0][1] == len(heat_loss_map[0]) - 1:
+        if min_distance > distance or min_distance < 0:
             min_distance = distance
 
 print(min_distance)
