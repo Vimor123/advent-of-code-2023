98628a413e5bab0a2ac3d88e9ab9db1e8f4f6a69
Vimor123 <vmladar1@gmail.com>
2023-12-25 08:28:51 +0100

    Huge thanks to HyperNeutrino

--- day24/task2.py
+++ day24/task2.py
@@ -1,6 +1,10 @@
+# Credit: HyperNeutrino
+
+import sympy
+
 hailstones = []
 
-input_file = open("input1.txt", "r")
+input_file = open("input.txt", "r")
 
 for line in input_file:
     position, velocity = line.strip().split(" @ ")
@@ -10,3 +14,15 @@ for line in input_file:
 
 input_file.close()
 
+
+p_x, p_y, p_z, v_x, v_y, v_z = sympy.symbols("p_x, p_y, p_z, v_x, v_y, v_z")
+
+equations = []
+
+for hailstone in hailstones:
+
+    equations.append((p_x - hailstone["position"][0]) * (hailstone["velocity"][1] - v_y) - (p_y - hailstone["position"][1]) * (hailstone["velocity"][0] - v_x))
+    equations.append((p_y - hailstone["position"][1]) * (hailstone["velocity"][2] - v_z) - (p_z - hailstone["position"][2]) * (hailstone["velocity"][1] - v_y))
+
+answer = sympy.solve(equations)[0]
+print(answer[p_x] + answer[p_y] + answer[p_z])
